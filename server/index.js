b78004e93f3be11c732199b6576578b9148f14d4
Timor Rust <timorust1@gmail.com>
2022-06-20 10:46:55 +0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('config')
+const authRouter = require('./routes/auth.routes')
+const app = express()
+const PORT = config.get('serverPort')
+
+app.use(express.json())
+app.use('/api/auth', authRouter)
+
+const start = async () => {
+	try {
+		await mongoose.connect(config.get('dbUrl'))
+
+		app.listen(PORT, () => {
+			console.log('server started... ', PORT)
+		})
+	} catch (error) {
+		
+	}
+}
+start()
